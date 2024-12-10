52188ef3fe83f3a020ee08e0928f90ce4a4a9985
sreeja o s <sreejaosreenivasan@gmail.com>
2024-12-10 22:07:54 +0530

    change

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,30 +1,22 @@
 import Stripe from 'stripe'; 
+import dotenv from 'dotenv';
 
-const stripe = new Stripe('sk_test_51QTRAELEKPpFnzksfYyTwdBcSxQq5EwTrRIVCm9dSOSAwPTCaHaUWPgPyJBHgLsPOXQhHkBryilrQnu1ug38W8V000FYXL0MNU', {
+dotenv.config();
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: '2022-11-15', 
-})
+});
 
-const paymentdata=async (req, res) => {
-    const { amount } = req.body;
-  
-    try {
-        const paymentIntent = await stripe.paymentIntents.create({
-            amount, 
-            currency: 'usd', 
-            payment_method_types: ['card'],
-            payment_method_options: {
-              card: {
-                request_three_d_secure: 'automatic',
-              },
-            },
-          });
-          
-  
-      res.status(200).json(paymentIntent.client_secret);
-    } catch (error) {
-      console.error('Error creating payment intent:', error.message);
-      res.status(500).json({ error: error.message });
-    }
-  };
-
-  export default paymentdata;
+export const paymentdata = async (req, res) => {
+  const { amount } = req.body;
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount, 
+      currency: 'usd', 
+      payment_method_types: ['card'],
+    });
+    res.status(200).json({ clientSecret: paymentIntent.client_secret });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import userRoutes from './routes/userRoutes.js';
 import cors from 'cors';
 import foodRoutes from './routes/foodRoutes.js';
 import orderRouter from './routes/orderRoutes.js';
-import bodyParser from 'body-parser';
 import paymentRouter from './routes/paymentRoutes.js';
 
 const app = express();
@@ -18,10 +17,11 @@ app.use(
   cors({
     origin:  ['http://localhost:3000', 'https://gofood-gz2h1nq3t-sreeja-sreenivasans-projects.vercel.app'], 
     credentials: true,
+    methods: ['GET', 'POST'], 
+    allowedHeaders: ['Content-Type'],
   })
 );
 app.use(express.json());
-app.use(bodyParser.json());
 
 
 const startServer = async () => {
