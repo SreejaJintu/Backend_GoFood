a4fb2497267e0f2115181897a99c03da486e752f
sreeja o s <sreejaosreenivasan@gmail.com>
2025-01-03 16:50:26 +0530

    updation2

--- models/Report.js
+++ models/Report.js
@@ -1,21 +1,30 @@
 import mongoose from "mongoose";
+import { v4 as uuidv4 } from "uuid"; 
 
 const reportSchema = new mongoose.Schema(
   {
+    reportId: {
+      type: String,
+      default: uuidv4, 
+      unique: true, 
+      required: true,
+    },
     title: {
-        type: String,
-        required: true,
-      },
-      description: {
-        type: String,
-        required: true,
-      },
-      generatedOn: {
-        type: Date,
-        default: Date.now,
-      },
-});
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    generatedOn: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true } 
+);
 
 const reportModel = mongoose.models.reports || mongoose.model("reports", reportSchema);
 
-export  { reportModel };
+export { reportModel };
